359d0c008863c9ef389b27eda25f1f316a050b94
hodtien <hodtienbrian@gmail.com>
2021-04-08 15:11:42 +0700

    update jsonparse.set

--- jsonparse/set.go
+++ jsonparse/set.go
@@ -6,10 +6,8 @@ import (
 	"github.com/buger/jsonparser"
 )
 
-func Set(data, value interface{}, keys ...string) error {
+func Set(data, value interface{}, keys ...string) ([]byte, error) {
 	dataBytes, _ := json.Marshal(data)
 	valueBytes, _ := json.Marshal(value)
-	_, err := jsonparser.Set(dataBytes, valueBytes, keys...)
-	
-	return err
+	return jsonparser.Set(dataBytes, valueBytes, keys...)
 }
